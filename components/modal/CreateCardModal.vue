eb892cc8d531ce4c949669b930bfb0cbbcb7c80d
hslee <hslee@concepters.co.kr>
2020-05-12 19:39:43 +0900

    layout commit

--- components/modal/CreateCardModal.vue
+++ components/modal/CreateCardModal.vue
@@ -2,10 +2,32 @@
     <div class="template">
         <div class="card-modal-template">
             <div class="flex-template-body">
-                <div>                    
-                    <a-icon type="close" @click="close()"/>
+                <div class="left-top-layout">
+                    <div class="img-box">
+
+                    </div>
+                </div>    
+                <div class="right-top-layout">
+                    <div class="top-tab-area">
+                        <a-icon class="round-close-btn" type="close-circle" @click="close()"/>
+                    </div>
+                    <!-- <a-icon type="close" @click="close()"/> -->
                 </div>
-                <div></div>
+                <div class="left-bot-layout">
+                    <div>
+                        <div class="search-modal-btn">
+
+                        </div>
+                        <div class="img-upload-btn">
+
+                        </div>
+                        <div class="form-btn">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="right-bot-layout">
+                    </div>                
             </div>
             
         </div>
@@ -48,18 +70,83 @@ export default {
         top: 50%;
         transform: translate(-50%, -50%);
         border-radius: 29px;
-        >.flex-template-body{
-            // display: flex;
-            // width: 100%;
-            // height: 100%;            
-            // margin:16px;            
+        >.flex-template-body{       
             display: flex;
-            align-items: center;
-            flex-direction: column;
-            background: wheat;
+            flex-wrap: wrap;
+            align-content: flex-start;
             border-radius: 29px;
-            padding: 16px;
+            padding: 32px;
+            padding-top: 16px;
             height: 100%;
+            >div{
+                height: 10px;
+                border: 1px solid gray;
+                padding: 16px;
+            }
+            >.left-top-layout{
+                display: flex;
+                justify-content: center;
+                flex-basis: 40%;
+                flex-grow: 1;
+                height: 70%;
+                padding-bottom: 0;
+                >.img-box{
+                    border: 1px solid gray;
+                    width: 80%;
+                    height: 100%;
+                }
+            }.right-top-layout{
+                display: flex;
+                flex-basis: 60%;
+                flex-grow: 1;
+                height: 70%;
+                >.top-tab-area{
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: flex-start;
+                    width: 100%;
+                    height: 15%;
+                    border: 1px solid gray;
+                    >.round-close-btn{
+                        font-size: 30px;
+                        margin-left: auto;
+                    }  
+                }      
+            }
+            >.left-bot-layout{
+                display: flex;
+                justify-content: center;
+                flex-basis: 40%;
+                height: 30%;
+                >div{
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                    border: 1px solid gray;
+                    width: 80%;
+                    height: 100%;
+                    >.search-modal-btn{
+                        border: 1px solid gray;
+                        width: 100%;
+                        height: 20%;
+                    }
+                    >.img-upload-btn{
+                        width: 100%;
+                        border: 1px solid gray;
+                        height: 20%;
+                    }
+                    >.form-btn{
+                        border: 1px solid gray;
+                        width: 100%;
+                        height: 40%;
+                    }
+                }
+            }
+            >.right-bot-layout{
+                flex-basis: 60%;
+                height: 30%;
+                
+            }
         }
     }
 }
